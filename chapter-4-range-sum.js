207c32fa370a5d2f145000b7c075982535989090
ytokisd <ytokisd@gmail.com>
2018-11-09 12:45:38 +0200

    Update chapter-4-range-sum.js

--- chapter-4-range-sum.js
+++ chapter-4-range-sum.js
@@ -1,17 +1,25 @@
-function range(start, finish) {
-	var theRangeArray = [];
-    var num = start;
-    for (i = 0; i <= finish - start; i++) {
-    	theRangeArray.push(num);
-      num ++;
-    };
+function range(start, finish, step) {
+  if ((start > finish && step >= 0) || (start < finish && step <= 0) ){
+    console.log("This won't work");
+    return false;
+  };
+    
+  var theRangeArray = [];
+  var countAbsolute = Math.abs(finish - start)
+  var step = step || 1;
+  var num = start;
+  var stepAbsolute = Math.abs(step)
+  for (i = 0; i <= countAbsolute; i = i + stepAbsolute ) {
+    theRangeArray.push(num);
+      num += step;
+  };
   return theRangeArray
 };
 
 function sum(array) {
   total = 0;
-	for (i = 0; i < array.length; i++){
- 		total += array[i];
+  for (i = 0; i < array.length; i++){
+    total += array[i];
   }
   return total
 }
