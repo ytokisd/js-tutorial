2c46709db9ec4193c842f82e9cbf45334a04dc43
ytokisd <ytokisd@gmail.com>
2018-11-13 15:58:32 +0200

    Create chapter-4-array-to-list-and-shit.js

---
+++ chapter-4-array-to-list-and-shit.js
@@ -0,0 +1,37 @@
+function arrayToList(array) {
+  ar = array.slice(0);
+  if (ar.length > 0) {
+    var lst = {value: ar.splice(0, 1)[0], rest: arrayToList(ar)};
+    return lst;
+  };
+};
+
+var arr = [];
+
+function listToArray(list) {
+  val = (list.value);
+  arr.push(val);
+  if (list.rest) {
+  	listToArray(list.rest);
+  };
+  return arr;
+};
+
+function prepend(val, list) {
+	var lst = {value: val, rest: list};
+  	return lst
+};
+
+var val
+function nth(list, position) {
+  		val = list.value
+		if (position != 0) {
+          nth(list.rest, position -1)
+		};
+  	return val;
+};
+
+console.log(prepend(10, prepend(20, null)));
+console.log(arrayToList([1, 2, 3]));
+console.log(listToArray(arrayToList([1, 2, 3])))
+console.log(nth(arrayToList([10, 20, 30]), 1));
