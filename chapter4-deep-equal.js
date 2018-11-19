e87b9d8b58275e91d21055fdb645083ab63db389
ytokisd <ytokisd@gmail.com>
2018-11-19 09:19:26 +0200

    Update and rename deep-equal.js to chapter4-deep-equal.js

---
+++ chapter4-deep-equal.js
@@ -0,0 +1,34 @@
+//Оператор == сравнивает переменные объектов, проверяя, ссылаются ли они на один объект.
+//Но иногда полезно было бы сравнить объекты по содержимому.
+
+//Напишите функцию deepEqual, которая принимает два значения и возвращает true, только если это два одинаковых значения или это объекты,
+//свойства которых имеют одинаковые значения, если их сравнивать рекурсивным вызовом deepEqual.
+
+//Чтобы узнать, когда сравнивать величины через ===, а когда – объекты по содержимому, используйте оператор typeof.
+//Если он выдаёт “object” для обеих величин, значит нужно делать глубокое сравнение. Не забудьте об одном дурацком исключении,
+//случившемся из-за исторических причин: “typeof null” тоже возвращает “object”.
+
+function deepEqual(a, b) {
+  if (a === b) return true;
+  
+  if (a == null || typeof a != "object" ||
+      b == null || typeof b != "object") return false;
+
+  let keysA = Object.keys(a), keysB = Object.keys(b);
+
+  if (keysA.length != keysB.length) return false;
+
+  for (let key of keysA) {
+    if (!keysB.includes(key) || !deepEqual(a[key], b[key])) return false;
+  }
+
+  return true;
+}
+
+let obj = {here: {is: "an"}, object: 2};
+console.log(deepEqual(obj, obj));
+// → true
+console.log(deepEqual(obj, {here: 1, object: 2}));
+// → false
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+// → true
